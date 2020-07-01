26a024b46c67f74f17b5323f2234d16a19f2c7aa
harrylb14 <harry.lingardbright@gmail.com>
2020-07-01 09:45:50 +0000

    Added rule and method for multiples of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,7 @@ describe 'fizzbuzz' do
   it "returns number when 4 passed as an arguement" do
     expect(fizzbuzz(4)).to eq 4 
   end 
+  it "returns 'buzz' when 5 is passed as an argument" do 
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
 end 
