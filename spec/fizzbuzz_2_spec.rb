05cc8765e987109288aaabd3d0f2073d5b0250c8
Ralph Mallett <rmallett12@gmail.com>
2020-07-01 10:13:30 +0000

    Add tests for class version of fizzbuzz

--- spec/fizzbuzz_2_spec.rb
+++ spec/fizzbuzz_2_spec.rb
@@ -3,4 +3,13 @@ describe 'fizzbuzz_2' do
   it 'returns "fizz" when 3 passed as an argument' do
     expect(3.fizzbuzz).to eq "fizz"
   end
+  it 'returns "fizz" when 5 passed as an argument' do
+    expect(5.fizzbuzz).to eq "buzz"
+  end
+  it 'returns "fizz" when 15 passed as an argument' do
+    expect(15.fizzbuzz).to eq "fizzbuzz"
+  end
+  it 'returns self when 4 passed as an argument' do
+    expect(4.fizzbuzz).to eq 4
+  end
 end
