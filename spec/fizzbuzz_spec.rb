95de7173028f67a9831b009891a56a7a77cb35be
Ralph Mallett <rmallett12@gmail.com>
2020-07-01 09:43:06 +0000

    Add test to return number when not divisble by 3 and update method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,7 @@ describe 'fizzbuzz' do
   it "returns 'fizz' when 3 passed as an arguement" do
     expect(fizzbuzz(3)).to eq "fizz"  
   end 
+  it "returns number when 4 passed as an arguement" do
+    expect(fizzbuzz(4)).to eq 4 
+  end 
 end 
