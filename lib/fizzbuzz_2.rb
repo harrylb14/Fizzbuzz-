cb3f4f47fac77e0866668877689aa3f18874829e
harrylb14 <harry.lingardbright@gmail.com>
2020-07-01 10:10:10 +0000

    Includes Fizzbuzz_2 using classes

---
+++ lib/fizzbuzz_2.rb
@@ -0,0 +1,14 @@
+class Integer
+
+def fizzbuzz
+  if self % 5 == 0 && self % 3 == 0
+    "fizzbuzz"
+  elsif self % 3 == 0
+    "fizz"
+  elsif self % 5 == 0 
+    "buzz"
+  else
+    self
+  end 
+end
+end
---
+++ spec/fizzbuzz_2_spec.rb
@@ -0,0 +1,6 @@
+require 'fizzbuzz_2'
+describe 'fizzbuzz_2' do
+  it 'returns "fizz" when 3 passed as an argument' do
+    expect(3.fizzbuzz).to eq "fizz"
+  end
+end
