611591ed2d7a27123ec5b8381f1ceb40536e178d
harrylb14 <harry.lingardbright@gmail.com>
2020-07-01 10:29:00 +0000

    tidy up

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,9 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it "returns 'fizz' when 3 passed as an arguement" do
+  it "returns 'fizz' when 3 passed as an argument" do
     expect(fizzbuzz(3)).to eq "fizz"  
   end 
-  it "returns number when 4 passed as an arguement" do
+  it "returns number when 4 passed as an argument" do
     expect(fizzbuzz(4)).to eq 4 
   end 
   it "returns 'buzz' when 5 is passed as an argument" do 
